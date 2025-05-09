4f7a3dd12fb9bb22a490dbf7c1421a677b6638dd
aditi1305-raut <158252121+aditi1305-raut@users.noreply.github.com>
2025-05-09 13:36:41 +0530

    Delete Assignment1_1.py

--- Assignment1_1.py
+++
@@ -1,9 +0,0 @@
-'''
-Write a program which contains one function named as Fun().That function should display 
-" Hello From Fun" on console.
-'''
-
-def Fun():
-    print("Hello from Fun.")
-
-Fun()
