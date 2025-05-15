9fe61741137e272d9e3e54deaada1ace0badb3cc
aditi1305-raut <158252121+aditi1305-raut@users.noreply.github.com>
2025-05-15 13:06:04 +0530

    Add files via upload

---
+++ Assignment1_10.py
@@ -0,0 +1,8 @@
+'''
+Write a program which accepts name from user and display length of its name.
+'''
+
+print("Enter a Name: ")
+name = str(input())
+
+print("The length of Name: ",len(name))
---
+++ Assignment1_8.py
@@ -0,0 +1,9 @@
+'''
+Write a Program which accept number from user and print that number of "*"
+on Screen .
+'''
+
+print("Enter the number of '*' : ")
+nums = int(input())
+
+print("* "*nums)
